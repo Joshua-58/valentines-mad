5b98d4ea725c2a51aa584673d25f5b8c10aa9eae
farsoomrohan <118229631+farsoomrohan@users.noreply.github.com>
2024-02-14 13:28:32 -0500

    Update main.dart - assets

--- main.dart
+++ main.dart
@@ -1,49 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
-
-class CustomShapeClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.lineTo(0, size.height);
-    path.quadraticBezierTo(size.width / 2, size.height - 100, size.width, size.height);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
-}
-
-class CustomShapePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint();
-    paint.color = Colors.red;
-    final path = Path();
-    path.lineTo(0, size.height * 0.6);
-    path.quadraticBezierTo(size.width / 2, size.height, size.width, size.height * 0.6);
-    path.lineTo(size.width, 0);
-    path.close();
-    canvas.drawPath(path, paint);
-  }
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Image Display',
       home: Scaffold(
-        body: CustomPaint(
-          painter: CustomShapePainter(),
-          child: Container(),
+        appBar: AppBar(
+          title: Text('Image Display'),
+        ),
+        body: Center(
+          child: Image.asset(
+            'assets/heart.jpg', // Change 'your_image_name.jpg' to the actual name of your image file
+            width: 200, // Adjust width as needed
+            height: 200, // Adjust height as needed
+            fit: BoxFit.contain, // Adjust the fit as needed
+          ),
         ),
       ),
     );
